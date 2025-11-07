e642bb4885d5595db0044c2d78ecd54927434e73
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-11-07 15:05:01 +0800

    sign in/sign up forms

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,21 @@
+const db = require("../db/queries");
+
 exports.renderHomePage = async (req, res) => {
   res.render("index");
+};
+
+exports.renderSignUp = async (req, res) => {
+  res.render("signup");
+};
+
+exports.signupPost = async (req, res, next) => {
+  // sign up
+};
+
+exports.renderSignin = async (req, res) => {
+  res.render("signin");
+};
+
+exports.signinPost = async (req, res, next) => {
+  // sign in
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,4 +5,9 @@ const indexRouter = Router();
 
 indexRouter.get("/", indexController.renderHomePage);
 
+indexRouter.get("/signup", indexController.renderSignUp);
+indexRouter.post("/signup", indexController.signupPost);
+indexRouter.get("/signin", indexController.renderSignIn);
+indexRouter.post("/signin", indexController.signinPost);
+
 module.exports = indexRouter;
