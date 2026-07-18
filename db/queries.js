f8c60c8525e32d3c99dd488002201074da96591a
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2026-07-18 15:11:07 +0800

    duplicate imports fixed

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,6 @@ const { fileLoader } = require("ejs");
 const dotenv = require("dotenv");
 const { Pool } = require("pg");
 const { PrismaPg } = require("@prisma/adapter-pg");
-const { PrismaClient } = require("@prisma/client");
 
 const db_URL = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
 
